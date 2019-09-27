cf8b116c43abb6f0d501fa9340caf9e930858280
vaibhav@1202 <agrawalvaibhav12@gmail.com>
2019-09-28 02:15:28 +0530

    Updates

---
+++ precache-manifest.870e6031d7bbdb223669cda555be9060.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1160712e0c786517345ac5451f2dc41b",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "20c1efdb850f945acc74",
+    "url": "/myapp/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "9ff84849ccdeed14444d",
+    "url": "/myapp/static/css/main.1a32cfb3.chunk.css"
+  },
+  {
+    "revision": "20c1efdb850f945acc74",
+    "url": "/myapp/static/js/2.0ad5f710.chunk.js"
+  },
+  {
+    "revision": "9ff84849ccdeed14444d",
+    "url": "/myapp/static/js/main.fb95f5e7.chunk.js"
+  },
+  {
+    "revision": "5b11d329a48cb2b3b216",
+    "url": "/myapp/static/js/runtime-main.dc287fc7.js"
+  },
+  {
+    "revision": "c123ef6a6c28bfdefa6e7404387b462b",
+    "url": "/myapp/static/media/myimgcircle.c123ef6a.png"
+  },
+  {
+    "revision": "b7cc0ac6cad680af0ce3f835346c1860",
+    "url": "/myapp/static/media/myphoto.b7cc0ac6.jpg"
+  }
+]);
